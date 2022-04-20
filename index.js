d3fbafb456587d78eb9b126529e2d2cc7b5e2370
gusdn0108 <gusdn6671@naver.com>
2022-04-20 21:15:47 +0900

    c

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('hi')
+import express from 'express'
+import cors from 'cors'
+import env from 'dotenv'
+import http from 'http'
+import route from './route'
+import path from 'path'
+env.config()
+
+const app = express();
+const server = http.createServer(app);
+
+
+const port = process.env.PORT || 3005
+app.use('/api',route)
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+})
+
+app.listen(port,()=>{
+    console.log('하이')
+})
